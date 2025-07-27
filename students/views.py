d2f21b7907e3c82192ca1328f2e005ade4c7a6fb
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-07-28 01:29:29 +0530

    made a json formate list named as students

--- students/views.py
+++ students/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def StudentList(reuqest):
-    return HttpResponse("this is student list")
+    students=[
+        {'id':1,
+        'name':'John Doe',
+        'age':20}
+    ]
+    # return HttpResponse("this is student list")
+    return HttpResponse(students)

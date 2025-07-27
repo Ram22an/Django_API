9f937a23c4dc84fb94ede2c39ada0b06b54021de
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-07-28 02:02:46 +0530

    made the model and migrated it

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Student
 # Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display=('student_id','name')
+admin.site.register(Student,StudentAdmin)

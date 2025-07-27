cf053483989f7bc08ba6266ec96ce389351c1780
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-07-28 01:50:35 +0530

    sent the json response

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
+def studentView(request):
+    Student={
+        'Id':1,
+        'name':'raman',
+        'class':'Computer Science'
+    }
+    return JsonResponse(Student)

9d4b06c41ee434e424f0b4456dc839079e043708
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-07-30 01:27:35 +0530

    made some changes in model and migrated it

--- employees/models.py
+++ employees/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
-    emp_id=models.CharField(max_length=20)
+    emp_id=models.CharField(max_length=20,unique=True)
     emp_name=models.CharField(max_length=50)
     designation=models.CharField(max_length=50)
 

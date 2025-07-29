435a4b39724cff4d15a67af5ca9d38efb55d7414
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-07-30 01:15:02 +0530

    made an employee model and migrated it

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Employee
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=('emp_id','emp_name')
+admin.site.register(Employee,EmployeeAdmin)
 # Register your models here.

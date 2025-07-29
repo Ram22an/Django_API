122d47f48ded88483aea3fe0139e236af7b50cfb
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-07-30 01:32:16 +0530

    created serializer for employee

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,16 @@
 from rest_framework import serializers
 from students.models import Student
+from employees.models import Employee
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model=Student
         fields="__all__"
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Employee
+        fields=['emp_id','emp_name','designation']
+
+
+

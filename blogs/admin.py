bbaeeb7277ee72a70647dc6fb98a5ed4e4b3329a
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-08-01 01:54:00 +0530

    made blog and comment model and migrated them

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Blogs,Comments
 # Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    list_display=('blog_title',)
+
+admin.site.register(Blogs,BlogAdmin)
+admin.site.register(Comments)
+
--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Blogs(models.Model):
+    blog_title=models.CharField(max_length=100)
+    blog_body=models.TextField()
+
+    def __str__(self):
+        return self.blog_title
+class Comments(models.Model):
+    # CASCADE is a rule in databases that automatically deletes related records when a record they depend on is deleted.
+    blog=models.ForeignKey(Blogs,on_delete=models.CASCADE,related_name='comments')
+    # ForeignKey is creating one(Blogs) to many(Comments) relation here
+    # there are two more relation like this models.OneToOneField and models.ManyToManyField
+    
+    # about related_name'comments'
+    # The related_name='comments' attribute lets you access all comments 
+    # for a specific blog post directly from the blog object itself. 
+    # It creates a convenient reverse relationship.
+    comment=models.TextField()
+
+    def __str__(self):
+        return self.comment
+
+

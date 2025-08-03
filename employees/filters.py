68b83c6e9cf85f36e386cad2aadcc99082423473
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-08-04 01:33:42 +0530

    added name filter

--- employees/filters.py
+++ employees/filters.py
@@ -3,10 +3,14 @@ from .models import Employee
 
 
 class EmployeeFilter(django_filters.FilterSet):
+    # iexact is used to ge the exact field
     designation=django_filters.CharFilter(field_name='designation',lookup_expr='iexact')
+    
+    # icontains is used to check if any string is present in field
+    EmpName=django_filters.CharFilter(field_name='emp_name',lookup_expr='icontains')
     class Meta:
         model=Employee
-        fields=['designation']
+        fields=['designation','EmpName']
 
 
 

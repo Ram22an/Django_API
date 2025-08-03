80edcd5d58a5f6ffa3d7fb852c9152cb3f8ba38c
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-08-04 01:59:57 +0530

    added id_min and id_max filters to EmployeeFilter for employee ID range queries

--- employees/filters.py
+++ employees/filters.py
@@ -9,14 +9,28 @@ class EmployeeFilter(django_filters.FilterSet):
     # icontains is used to check if any string is present in field
     EmpName=django_filters.CharFilter(field_name='emp_name',lookup_expr='icontains')
     
-    # RangeFilter is used to get any field in range
+    # RangeFilter is used to get any field in range and it only work on integer field
     ID=django_filters.RangeFilter(field_name='id')
     
+    id_min=django_filters.CharFilter(method='filter_by_id_range',label='From EMP_ID')
+    id_max=django_filters.CharFilter(method='filter_by_id_range',label='To EMP_ID')
     class Meta:
         model=Employee
-        fields=['designation','EmpName','ID']
+        fields=['designation','EmpName','ID','id_min','id_max']
 
 
+    def filter_by_id_range(self,queryset,name,value):
+        # queryset: The current list of employees.
+        # name: The name of the filter that was used (either 'id_min' or 'id_max').
+        # value: The number the user typed in.
+        if name=='id_min':
+            # here emp_id is the field in model of employee
+            return queryset.filter(emp_id__gte=value)
+            # gte: Stands for Greater Than or Equal to (>=).
+        elif name=='id_max':
+            # lte: Stands for Less Than or Equal to (<=).
+            return queryset.filter(emp_id__lte=value)
 
+        return queryset
 
 
